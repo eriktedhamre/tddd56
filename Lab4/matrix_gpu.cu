545bbaa998b4f2edd4c9d5d282be7b7b2295ffb9
davva679 <davva679@student.liu.se>
2019-11-29 10:52:14 +0100

    Completed lab4

--- Lab4/matrix_gpu.cu
+++ Lab4/matrix_gpu.cu
@@ -9,14 +9,16 @@ void add_matrix(float *a, float *b, float *c, int N)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	int idy = blockIdx.y * blockDim.y + threadIdx.y;
-	printf("%d \n",idx * N + idy);
+	//printf("%d \n",idx * N + idy);
 	c[idx * N + idy] = a[idx * N + idy] + b[idx * N + idy];
-		/*
+	//c[idy * N + idx] = a[idy * N + idx] + b[idy * N + idx];
+/*
+// Bad pattern to access after each other since each thread has to read memory for each access and threads can not use one read for multiple data points
 	for (int i = 0; i < N; i++) {
-
 			c[idx * N + i] = a[idx * N + i] + b[idx * N + i];
+			//c[idx + N * i] = a[idx + N * i] + b[idx + N * i];
 	}
-	*/
+*/
 }
 /*
 cudaMalloc( (void**)&cd, size );
@@ -42,10 +44,10 @@ void add_matrix_cpu(float *a, float *b, float *c, int N)
 
 int main()
 {
-	const int N = 64;
-	const int BLOCKSIZE = 16;
-	const int GRIDSIZE = 4;
-	int size = N*N*sizeof(float);
+	const int N = 6144 ;
+	const int BLOCKSIZE = 32;
+	const int GRIDSIZE = 192;
+	long size = (long) N*N*sizeof(float);
 	float * a_h,* a_d,* b_h,* b_d,* c_d,* c_h;
 	a_h = (float *)malloc(size);
 	cudaMalloc((void **) &a_d, size);
@@ -82,8 +84,10 @@ int main()
 		cudaEventSynchronize(stop);
 
 		cudaEventElapsedTime(&GPUTime, start, stop);
-		printf("GPU time = %f", GPUTime);
 
+		cudaError_t err = cudaGetLastError();
+	  	if (err != cudaSuccess)
+	    	printf("Error: %s\n", cudaGetErrorString(err));
 		cudaMemcpy(c_h, c_d, size, cudaMemcpyDeviceToHost);
 
 		float * a, *b, *c;
@@ -94,27 +98,29 @@ int main()
 		for (int i = 0; i < N; i++){
 			for (int j = 0; j < N; j++)
 			{
-				a_h[i+j*N] = 10 + i;
-				b_h[i+j*N] = (float)j / N;
-				c_h[i+j*N] = 0;
+				a[i+j*N] = 10 + i;
+				b[i+j*N] = (float)j /N;
+				c[i+j*N] = 0;
 			}
 		}
 		int CPUTime;
 		GetMilliseconds();
 		add_matrix_cpu(a, b, c, N);
 		CPUTime = GetMilliseconds();
-		printf("CPU time = %d", CPUTime);
 
 
-/*
-	for (int i = 0; i < N; i++)
+
+	for (int i = 0; i < N; i+=1000)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < N; j+=1000)
 		{
 			printf("%0.2f ", c_h[i+j*N]);
+			printf("%0.2f ", c[i+j*N]);
 		}
 		printf("\n");
 	}
-	free(c_h);
-	*/
+
+	printf("GPU time = %f ms\n", GPUTime);
+	printf("CPU time = %d ms\n", CPUTime);
+
 }
