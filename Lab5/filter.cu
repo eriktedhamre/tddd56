a0eef0d66815ccf90e7acd14bc1766702f9cc1b8
davva679 <davva679@student.liu.se>
2019-12-10 10:02:36 +0100

    started working on first task of Lab5, problem with illegal memory access

--- Lab5/filter.cu
+++ Lab5/filter.cu
@@ -35,7 +35,7 @@
 
 
 __global__ void filter(unsigned char *image, unsigned char *out, const unsigned int imagesizex, const unsigned int imagesizey, const int kernelsizex, const int kernelsizey)
-{ 
+{
   // map from blockIdx to pixel position
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -44,18 +44,18 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
   unsigned int sumx, sumy, sumz;
 
   int divby = (2*kernelsizex+1)*(2*kernelsizey+1); // Works for box filters only!
-	
+
 	if (x < imagesizex && y < imagesizey) // If inside image
 	{
 // Filter kernel (simple box filter)
 	sumx=0;sumy=0;sumz=0;
 	for(dy=-kernelsizey;dy<=kernelsizey;dy++)
-		for(dx=-kernelsizex;dx<=kernelsizex;dx++)	
+		for(dx=-kernelsizex;dx<=kernelsizex;dx++)
 		{
 			// Use max and min to avoid branching!
 			int yy = min(max(y+dy, 0), imagesizey-1);
 			int xx = min(max(x+dx, 0), imagesizex-1);
-			
+
 			sumx += image[((yy)*imagesizex+(xx))*3+0];
 			sumy += image[((yy)*imagesizex+(xx))*3+1];
 			sumz += image[((yy)*imagesizex+(xx))*3+2];
@@ -65,6 +65,65 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
 	out[(y*imagesizex+x)*3+2] = sumz/divby;
 	}
 }
+
+__global__ void filterOptimized(unsigned char *image, unsigned char *out, const unsigned int imagesizex, const unsigned int imagesizey, const int kernelsizex, const int kernelsizey)
+{
+  // map from blockIdx to pixel position
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int dy, dx;
+  unsigned int sumx, sumy, sumz;
+
+  int divby = (2*kernelsizex+1)*(2*kernelsizey+1); // Works for box filters only!
+  extern __shared__ unsigned char sData[]; //size = (2*kernelsizex+1)*(2*kernelsizey+1)*3*sizeof(unsigned char)
+// use threadIdx.x and .y insetad of x and y since we are in a block that is executed on a MP?
+/*
+  sData[(y*imagesizex+x)*3+0] = image[(y*imagesizex+x)*3+0];
+  sData[(y*imagesizex+x)*3+1] = image[(y*imagesizex+x)*3+1];
+  sData[(y*imagesizex+x)*3+2] = image[(y*imagesizex+x)*3+2];
+
+  sData[((2*kernelsizex+1)*kernelsizey+1)*(y*imagesizex+x)*3+0] = image[(y*imagesizex+x)*3+0];
+  sData[((2*kernelsizex+1)*kernelsizey+1)*(y*imagesizex+x)*3+1] = image[(y*imagesizex+x)*3+1];
+  sData[((2*kernelsizex+1)*kernelsizey+1)*(y*imagesizex+x)*3+2] = image[(y*imagesizex+x)*3+2];
+*/
+
+// want to get x-threadidx and y-threadidy to get the values outside of the used grid
+  int imy = min(max(y-2*threadIdx.y,0), imagesizey-1);
+  int imx = min(max(x-2*threadIdx.x,0), imagesizex-1);
+  sData[(threadIdx.y*kernelsizex+threadIdx.x)*3+0] = image[(imy*imagesizex+x-2*imx)*3+0];
+  sData[(threadIdx.y*kernelsizex+threadIdx.x)*3+1] = image[(imy*imagesizex+x-2*imx)*3+1];
+  sData[(threadIdx.y*kernelsizex+threadIdx.x)*3+2] = image[(imy*imagesizex+x-2*imx)*3+2];
+
+  imy = min(max(y*imagesizex, 0), imagesizey-1);
+  imx = min(max(x,0), imagesizex-1);
+  sData[((2*kernelsizex+1)*kernelsizey+1)+(threadIdx.y*kernelsizex+threadIdx.x)*3+0] = image[(imy*imagesizex+imx)*3+0];
+  sData[((2*kernelsizex+1)*kernelsizey+1)+(threadIdx.y*kernelsizex+threadIdx.x)*3+1] = image[(imy*imagesizex+imx)*3+1];
+  sData[((2*kernelsizex+1)*kernelsizey+1)+(threadIdx.y*kernelsizex+threadIdx.x)*3+2] = image[(imy*imagesizex+imx)*3+2];
+
+  __syncthreads();
+  if (x < imagesizex && y < imagesizey) // If inside image
+	{
+// Filter kernel (simple box filter)
+	sumx=0;sumy=0;sumz=0;
+	for(dy=0;dy<=2*kernelsizey+1;dy++)
+		for(dx=0;dx<=2*kernelsizex+1;dx++)
+		{
+			// Use max and min to avoid branching!
+			int yy = min(max(threadIdx.y+dy, 0), 2*kernelsizey);
+			int xx = min(max(threadIdx.x+dx, 0), 2*kernelsizex);
+
+			sumx += sData[((yy)*imagesizex+(xx))*3+0];
+			sumy += sData[((yy)*imagesizex+(xx))*3+1];
+			sumz += sData[((yy)*imagesizex+(xx))*3+2];
+		}
+	out[(y*imagesizex+x)*3+0] = sumx/divby;
+	out[(y*imagesizex+x)*3+1] = sumy/divby;
+	out[(y*imagesizex+x)*3+2] = sumz/divby;
+    __syncthreads();
+	}
+}
+
 
 // Global variables for image data
 
@@ -87,21 +146,31 @@ void computeImages(int kernelsizex, int kernelsizey)
 	cudaMemcpy( dev_input, image, imagesizey*imagesizex*3, cudaMemcpyHostToDevice );
 	cudaMalloc( (void**)&dev_bitmap, imagesizex*imagesizey*3);
 	dim3 grid(imagesizex,imagesizey);
-	filter<<<grid,1>>>(dev_input, dev_bitmap, imagesizex, imagesizey, kernelsizex, kernelsizey); // Awful load balance
+  filter<<<grid,1>>>(dev_input, dev_bitmap, imagesizex, imagesizey, kernelsizex, kernelsizey); // Awful load balance
 	cudaThreadSynchronize();
 //	Check for errors!
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess)
         printf("Error: %s\n", cudaGetErrorString(err));
 	cudaMemcpy( pixels, dev_bitmap, imagesizey*imagesizex*3, cudaMemcpyDeviceToHost );
-	cudaFree( dev_bitmap );
+  unsigned char* sharedData;
+  cudaMalloc((void**)&sharedData, (kernelsizex*2+1)*(kernelsizey*2+1)*3*sizeof(unsigned char));
+  filterOptimized<<<grid, 1, (kernelsizex*2+1)*(kernelsizey*2+1)*3*sizeof(unsigned char)>>>(dev_input, dev_bitmap, imagesizex, imagesizey, kernelsizex, kernelsizey);
+  cudaThreadSynchronize();
+  //	Check for errors!
+    err = cudaGetLastError();
+    if (err != cudaSuccess)
+        printf("Optimized version Error: %s\n", cudaGetErrorString(err));
+	cudaMemcpy( pixels, dev_bitmap, imagesizey*imagesizex*3, cudaMemcpyDeviceToHost );
+  cudaFree( sharedData );
+  cudaFree( dev_bitmap );
 	cudaFree( dev_input );
 }
 
 // Display images
 void Draw()
 {
-// Dump the whole picture onto the screen.	
+// Dump the whole picture onto the screen.
 	glClearColor( 0.0, 0.0, 0.0, 1.0 );
 	glClear( GL_COLOR_BUFFER_BIT );
 
@@ -123,7 +192,7 @@ void Draw()
 }
 
 // Main program, inits
-int main( int argc, char** argv) 
+int main( int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
